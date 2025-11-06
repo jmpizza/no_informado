094860dd4b271065a8d5a56aaf802e8935f8edce
w15hy <ingw15hy@gmail.com>
2025-11-05 22:28:41 -0500

    Medios de pago UI parte 1

--- Project/src/App.jsx
+++ Project/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
             ) : (
                 <div className="flex h-screen w-screen">
                     <LeftPanel setView={setView} />
-                    <div className="bg-black flex-1 p-6">
+                    <div className="bg-white flex-1 p-20">
                         {view === "dashboard" && <Dashboard />}
                         {view === "mediosPago" && <MediosPago />}
                     </div>
